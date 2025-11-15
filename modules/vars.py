374fd35cf4ee5b012a43a201c075dc5de324df53
sumitkumawat090912-dot <sumitkumawat090912@gmail.com>
2025-11-15 19:58:23 +0530

    main

--- modules/vars.py
+++ modules/vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "29899250"))
 API_HASH = environ.get("API_HASH", "611d045796c79af3e5ddfa3d6fd536a7")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8301968841:AAFnD7n1_8mheKMom7KGP3eI2MwJEEY5d-8")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8301968841:AAFvYiIZPrS5iHBk628oyflCnxcs68E_jgo")
 
 OWNER = int(environ.get("OWNER", "8251679097"))
 CREDIT = environ.get("CREDIT", "@ll_kumawat_ll")
